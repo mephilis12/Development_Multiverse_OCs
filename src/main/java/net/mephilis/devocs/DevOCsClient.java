dc7d55e030799d16a32603e74b3bc1666918f8ab
mephilis12 <chillybilly2000@yahoo.com>
2024-11-22 01:30:10 +0200

    I did stuff

---
+++ src/main/java/net/mephilis/devocs/DevOCsClient.java
@@ -0,0 +1,10 @@
+package net.mephilis.devocs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DevOCsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
